35c6f1e9f7c313251753060843e2dd1bc43d3b64
Rares <rares_lungescu@yahoo.com>
2021-01-24 15:38:44 +0200

    Update index to export all animations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import transitions2d from "./transitions/Transitions2D";
 import backgroundTransitions from "./transitions/BackgroundTransitions";
+import borderTransitions from "./transitions/BorderTransitions";
+import shadowTransitions from "./transitions/ShadowTransitions";
+import speechBubblesTransitions from "./transitions/SpeechBubblesTransitions";
 
-export { transitions2d, backgroundTransitions };
+export {
+  transitions2d,
+  backgroundTransitions,
+  borderTransitions,
+  shadowTransitions,
+  speechBubblesTransitions,
+};
